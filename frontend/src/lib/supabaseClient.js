f14a25aa3482ff661e02e18cde27d1ce291177ee
byna81 <fofana.naby81@gmail.com>
2026-02-16 10:37:02 +0100

    Update supabaseClient.js

--- frontend/src/lib/supabaseClient.js
+++ frontend/src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
-// supabaseClient.js - Configuration client Supabase
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://xnccwqnayjgxgdgiwcnx.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhuY2N3cW5heWpneGdkZ2l3Y254Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcxNTE1NTcsImV4cCI6MjA1MjcyNzU1N30.r7V3cqcHqLSE5YlZGRMXOTpJhкултиваYLASdX8kfGMI'
+
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhuY2N3cW5heWpneGdkZ2l3Y254Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5NzIwMTcsImV4cCI6MjA4NjU0ODAxN30.EW4i3vN1jKdEPtwkimT8Bxfc9bUQQboyTaHLyX0a-t4'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
